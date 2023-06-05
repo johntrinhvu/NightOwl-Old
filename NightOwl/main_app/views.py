e2e06ec378143c34319751f070071f0cb3c4d784
gambitpolizei-ga <timothyjohnroncal@gmail.com>
2023-06-05 12:01:05 -0700

    add folders

--- NightOwl/main_app/views.py
+++ NightOwl/main_app/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main_app index.")
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
