f7028a8878bf141582fab221ccc00456b87a3ec6
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-06-06 17:30:55 -0500

    fixed loginreg

--- NightOwl/NightOwl/settings.py
+++ NightOwl/NightOwl/settings.py
@@ -134,12 +134,10 @@ LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/'
 
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/signup/'
 
 
 # LOGIN_REDIRECT_URL = '/events/'
-
-LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- NightOwl/main_app/urls.py
+++ NightOwl/main_app/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', LoginView.as_view(template_name='nightowl/login.html'), name='login_view'),
-    path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
 ]
