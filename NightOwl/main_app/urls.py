39c02aaeb2ec6eaba2bab4c61b3c59df2f2a7210
gambitpolizei-ga <timothyjohnroncal@gmail.com>
2023-06-06 14:01:06 -0700

    merge conflict

--- NightOwl/main_app/urls.py
+++ NightOwl/main_app/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns = [
     path('events/', views.events_index, name='index'),
     path('', views.home, name='home'),
+    path('events/<int:event_id>/', views.events_detail, name='detail'),
+    path('events/create/', views.EventCreate.as_view(), name='events_create'),
+    path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
+    path('events/<int:pk>/delete/', views.EventDelete.as_view(), name='events_delete'),
     path('login/', LoginView.as_view(template_name='nightowl/login.html'), name='login_view'),
     
     path('about/', views.about, name='about'),
