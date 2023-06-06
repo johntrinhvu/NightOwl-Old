b6c5813d5ab6838c601bb459248286e8add8b097
babel-17 <chicas8414@yahoo.com>
2023-06-06 15:23:00 -0700

    merge conflict fix

--- NightOwl/main_app/views.py
+++ NightOwl/main_app/views.py
@@ -1,12 +1,11 @@
 
-# import os
-# import uuid
-# import boto3
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
