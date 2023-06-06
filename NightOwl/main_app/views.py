12df558649aa5e30254613ee4f8c63a8f90056f8
John Vu <johntrinhvu@yahoo.com>
2023-06-06 12:10:20 -0700

    test

--- NightOwl/main_app/views.py
+++ NightOwl/main_app/views.py
@@ -1,13 +1,13 @@
-import os
-import uuid
-import boto3
+# import os
+# import uuid
+# import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Event, Photo
+from .models import Event
 
 
 
